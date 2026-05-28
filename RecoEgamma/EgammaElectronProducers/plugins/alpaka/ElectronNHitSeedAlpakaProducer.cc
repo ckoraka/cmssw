1aa59a6282fca78d66c51ece96d495d5ee39b136
Christian Wessel <christian.wessel@desy.de>
2026-05-28 15:02:28 +0200

    Move makeMatchingCutsPSetDescription to producer and delete header that included it so far.

--- RecoEgamma/EgammaElectronProducers/plugins/alpaka/ElectronNHitSeedAlpakaProducer.cc
+++ RecoEgamma/EgammaElectronProducers/plugins/alpaka/ElectronNHitSeedAlpakaProducer.cc
@@ -39,12 +39,37 @@
 #include "HeterogeneousCore/AlpakaInterface/interface/config.h"
 #include "HeterogeneousCore/AlpakaCore/interface/alpaka/global/EDProducer.h"
 
-#include "RecoEgamma/EgammaElectronAlgos/interface/TrajSeedMatchHelpers.h"
+// #include "RecoEgamma/EgammaElectronAlgos/interface/TrajSeedMatchHelpers.h"
 #include "RecoEgamma/EgammaElectronAlgos/interface/TrajSeedMatchingCutsPortable.h"
 
 #include "PixelMatchingAlgo.h"
 
 namespace {
+  edm::ParameterSetDescription makeMatchingCutsPSetDescription() {
+    edm::ParameterSetDescription desc;
+
+    edm::ParameterSetDescription cutsDesc;
+    cutsDesc.add<std::vector<double> >("dPhiMaxHighEt",       {0.003});
+    cutsDesc.add<std::vector<double> >("dPhiMaxHighEtThres",  {0.0});
+    cutsDesc.add<std::vector<double> >("dPhiMaxLowEtGrad",    {0.0});
+    cutsDesc.add<std::vector<double> >("dRZMaxHighEt",        {0.005});
+    cutsDesc.add<std::vector<double> >("dRZMaxHighEtThres",   {30});
+    cutsDesc.add<std::vector<double> >("dRZMaxLowEtGrad",     {-0.002});
+    cutsDesc.add<std::vector<double> >("etaBins",             {});
+
+    edm::ParameterSet defaults;
+    defaults.addParameter<std::vector<double> >("dPhiMaxHighEt",      std::vector<double>{0.003});
+    defaults.addParameter<std::vector<double> >("dPhiMaxHighEtThres", std::vector<double>{0.0});
+    defaults.addParameter<std::vector<double> >("dPhiMaxLowEtGrad",   std::vector<double>{0.0});
+    defaults.addParameter<std::vector<double> >("dRZMaxHighEt",       std::vector<double>{0.005});
+    defaults.addParameter<std::vector<double> >("dRZMaxHighEtThres",  std::vector<double>{30});
+    defaults.addParameter<std::vector<double> >("dRZMaxLowEtGrad",    std::vector<double>{-0.002});
+    defaults.addParameter<std::vector<double> >("etaBins",            std::vector<double>{});
+
+    desc.addVPSet("matchingCuts", cutsDesc, std::vector<edm::ParameterSet>{defaults, defaults, defaults});
+    return desc;
+  }
+
   template<size_t nMaxEtaBins, size_t nMaxHits>
   auto makeMatchingCuts(std::vector<edm::ParameterSet> const& cutsPSets) {
     std::array<egamma::MatchingCuts<nMaxEtaBins>, nMaxHits> matchingCuts;
@@ -227,7 +252,7 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
       desc.add<edm::InputTag>("initialSeeds", {"hltElePixelSeedsCombined"});
       desc.add<edm::InputTag>("beamSpot", {"hltOnlineBeamSpot"});
       desc.add<edm::InputTag>("superClusters", {"hltEgammaSuperClustersToPixelMatch"});
-      desc.add<edm::ParameterSetDescription>("matchingCuts", egamma::makeMatchingCutsPSetDescription());
+      desc.add<edm::ParameterSetDescription>("matchingCuts", makeMatchingCutsPSetDescription());
       descriptions.addWithDefaultLabel(desc);
     }
 
